91d65bc45de7d7542d2693e0a25231e7cb0ddb2d
Ayush Shrestha <60820123+ayushstha123@users.noreply.github.com>
2024-07-20 17:16:42 +0545

    Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import { ImgCaption } from './Pages/imgcaption.jsx'; // Ensure the correct path
-import './App.css';
+import { ImgCaption } from './Pages/imgcaption.jsx'; // Adjust the path if needed
+import './App.css'; // Optional, for additional styles
+import Header from './Components/Header.jsx'; // Ensure the correct path
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home.jsx';
 
 function App() {
   return (
     <div className="App">
-      <ImgCaption />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/objectdetection' element={<ImgCaption />} />
+          <Route path='/imgCaption' element={<ImgCaption />} />
+          {/* Add more routes here if needed */}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
