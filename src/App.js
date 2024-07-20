46b6fab5cb3285948f372cf73067667d3eb19583
Ayush Shrestha <60820123+ayushstha123@users.noreply.github.com>
2024-07-20 23:34:45 +0545

    voice transcription

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import './App.css'; // Optional, for additional styles
 import Header from './Components/Header.jsx'; // Ensure the correct path
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home.jsx';
+import VoiceAssist from './Components/VoiceAssist.jsx';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Header />
+      <VoiceAssist/>
+       
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/objectdetection' element={<ObjDetection />} />
