103fcecc52bf74b25565b5ded3763427753c1fe6
Saugat Joshi <jsaugatt02@gmail.com>
2024-07-21 08:52:36 +0545

    fix header icons

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,36 +1,36 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { CircleDollarSign, Home, Image, MapPinned } from 'lucide-react';
+import { CircleDollarSign, ScanLine, Home, Image, MapPinned, BotMessageSquare  } from 'lucide-react';
 
 const Header = () => {
   return (
     <nav className="bg-black border-t border-gray-200">
       <div className="flex justify-around">
-        <Link className="block text-black/60 transition duration-200 hover:text-black/80 dark:text-white/60 dark:hover:text-white/80 lg:px-2" to="/">
+        <Link className="block text-black/60 transition duration-200 hover:text-black/80 dark:text-white/60 dark:hover:text-white/80 lg:px-2" to="/objectdetection">
           <div className="flex flex-col items-center p-2 text-white hover:text-blue-500 active:text-blue-500">
-            <Home />
-            <p className="text-sm pt-2">Object</p>
+            <ScanLine strokeWidth="1.5px"/>
+            <p className="text-sm pt-2">Object Detection</p>
           </div>
         </Link>
 
         <Link className="block text-black/60 transition duration-200 hover:text-black/80 dark:text-white/60 dark:hover:text-white/80 lg:px-2" to="/imgcaption">
           <div className="flex flex-col items-center p-2 text-white hover:text-blue-500 active:text-blue-500">
-            <CircleDollarSign />
-            <p className="text-sm pt-2">Money</p>
+            <Image strokeWidth="1.5px"/>
+            <p className="text-sm pt-2">Image Caption</p>
           </div>
         </Link>
 
         <Link className="block text-black/60 transition duration-200 hover:text-black/80 dark:text-white/60 dark:hover:text-white/80 lg:px-2" to="/location">
           <div className="flex flex-col items-center p-2 text-white hover:text-blue-500 active:text-blue-500">
-            <MapPinned />
-            <p className="text-sm pt-2">Map</p>
+            <MapPinned strokeWidth="1.5px"/>
+            <p className="text-sm pt-2">Location</p>
           </div>
         </Link>
 
         <Link className="block text-black/60 transition duration-200 hover:text-black/80 dark:text-white/60 dark:hover:text-white/80 lg:px-2" to="/speechtotext">
           <div className="flex flex-col items-center p-2 text-white hover:text-blue-500 active:text-blue-500">
-            <Image />
-            <p className="text-sm pt-2">Gallery</p>
+            <BotMessageSquare  strokeWidth="1.5px"/>
+            <p className="text-sm pt-2">Chat Assistant</p>
           </div>
         </Link>
       </div>
